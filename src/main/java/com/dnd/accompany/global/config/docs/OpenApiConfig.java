55b7c4d3c7a4a4600b4c3d7ac0e4952f06be0fb5
rlarltj123 <rlarltj1234@gmail.com>
2024-07-18 01:07:24 +0900

    feat: Swagger UI 설정을 추가한다.

---
+++ src/main/java/com/dnd/accompany/global/config/docs/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.dnd.accompany.global.config.docs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+@Profile({"local", "dev", "prod"})
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI openApi() {
+		Info info = new Info()
+			.title("DND API Document");
+
+		String jwtSchemeName = "Authorization";
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+		Components components = new Components()
+			.addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+				.name(jwtSchemeName)
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("Bearer")
+				.bearerFormat("JWT"));
+
+		return new OpenAPI()
+			.addServersItem(new Server().url("/"))
+			.components(new Components())
+			.addSecurityItem(securityRequirement)
+			.components(components)
+			.info(info);
+	}
+}
